67eda0d8d14da3606d8a9d94f65027da4df5465f
4m4z1gh <aitbaha48@gmail.com>
2024-07-11 02:34:09 +0300

    ds

--- 2-datastructures/ds.go
+++ 2-datastructures/ds.go
@@ -7,12 +7,21 @@ func main() {
 	var twoD [2][3]int
 
 	letters := []string{"a", "b", "c", "d"}
+	letters2 := []string{}
 
-	/*  using make -> make([]T, len, cap) */
+	//Copy
+	copy(letters2, letters)
+
+	//Append Letter
+	letters = append(letters, "e")
+
+	//Delete Letter
+	letters = append(letters[:1], letters[2:]...)
+
 	var s []byte
 	s = make([]byte, 5, 5)
 	//OR
 	ss := make([]byte, 5)
 
-	fmt.Println(x, twoD, letters, s, ss)
+	fmt.Println(x, twoD, letters, s, ss, letters2)
 }
