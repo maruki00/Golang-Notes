688d515dfd72c32ff851826bd869c0f029ee4b31
maruki00 <aitbaha48@gmail.com>
2024-08-14 12:49:56 +0300

    --

--- 4-generics/main.go
+++ 4-generics/main.go
@@ -1,13 +1,19 @@
 package main
 
+import "fmt"
+
 type DefaultType interface {
 	int | float32 | float64
 }
 
-func GetDefault[V DefaultType]() V {
+func GetDefault[V DefaultType](items []V) V {
 	return 10
 }
 
 func main() {
-	for 
+	items1 := []int{1, 23, 54, 6456, 45645, 4564}
+	items2 := []float32{1.345, 23.345, 54.234, 6456.78, 45645.78, 4564.00}
+	fmt.Println("result : ", GetDefault[int](items1))
+	fmt.Println("result : ", GetDefault[float32](items2))
+
 }
